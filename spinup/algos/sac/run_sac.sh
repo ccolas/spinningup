c67a302be30b2123d923d92b26e43e4b1d66e789
Cédric Colas <cedric.colas@inria.fr>
2019-02-16 16:56:05 +0100

    new bash file

--- spinup/algos/sac/run_sac.sh
+++ spinup/algos/sac/run_sac.sh
@@ -6,9 +6,10 @@
 #SBATCH -o run_sac.sh.out
 rm log.txt; 
 export EXP_INTERP='/cm/shared/apps/intel/composer_xe/python3.5/intelpython3/bin/python3' ;
-$EXP_INTERP sac.py --env FetchPush-v1 --trial_id 0 --exp_name tests_sac & 
-$EXP_INTERP sac.py --env FetchPush-v1 --trial_id 1 --exp_name tests_sac & 
-$EXP_INTERP sac.py --env FetchPush-v1 --trial_id 2 --exp_name tests_sac & 
-$EXP_INTERP sac.py --env FetchPush-v1 --trial_id 3 --exp_name tests_sac & 
-$EXP_INTERP sac.py --env FetchPush-v1 --trial_id 4 --exp_name tests_sac & 
+$EXP_INTERP sac_uvfa.py --env FetchPush-v1 --trial_id 5 --exp_name tests_sac & 
+$EXP_INTERP sac_uvfa.py --env FetchPush-v1 --trial_id 6 --exp_name tests_sac & 
+$EXP_INTERP sac_uvfa.py --env FetchPush-v1 --trial_id 7 --exp_name tests_sac & 
+$EXP_INTERP sac_uvfa.py --env FetchPush-v1 --trial_id 8 --exp_name tests_sac & 
+$EXP_INTERP sac_uvfa.py --env FetchPush-v1 --trial_id 9 --exp_name tests_sac & 
+$EXP_INTERP sac_uvfa.py --env FetchPush-v1 --trial_id 10 --exp_name tests_sac & 
 wait
