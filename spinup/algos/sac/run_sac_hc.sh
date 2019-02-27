57525c97209b7059f765418af8960e04c096ccc0
Cédric Colas <cedric.colas@inria.fr>
2019-02-28 00:17:30 +0100

    modify bash

--- spinup/algos/sac/run_sac_hc.sh
+++ spinup/algos/sac/run_sac_hc.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 #SBATCH --mincpus 24
 #SBATCH -p longq
-#SBATCH -t 50:00:00
+#SBATCH -t 30:00:00
 #SBATCH -e run_sac_hc.sh.err
 #SBATCH -o run_sac_hc.sh.out
 rm log.txt; 
 export EXP_INTERP='/cm/shared/apps/intel/composer_xe/python3.5/intelpython3/bin/python3' ;
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 96 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 97 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 98 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 99 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 100 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 101 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 102 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 103 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 104 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 105 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 106 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 107 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 108 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 109 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 110 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 111 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 112 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 113 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 114 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 115 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 116 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 117 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 118 --exp_name sac_hc & 
-$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 119 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 0 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 1 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 2 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 3 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 4 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 5 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 6 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 7 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 8 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 9 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 10 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 11 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 12 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 13 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 14 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 15 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 16 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 17 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 18 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 19 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 20 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 21 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 22 --exp_name sac_hc & 
+$EXP_INTERP sac.py --env HalfCheetah-v2 --trial_id 23 --exp_name sac_hc & 
 wait
